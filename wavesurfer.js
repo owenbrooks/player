fe9fe4b4937d70de3cbe666d34862aa0ffa3e5c3
Owen Brooks <owen.h.brooks@gmail.com>
2024-02-11 16:39:26 +1100

    Add marks

--- wavesurfer.js
+++ wavesurfer.js
@@ -8,31 +8,33 @@ const wavesurfer = WaveSurfer.create({
 
 const marks = [0.0];
 
-// const wsRegions = wavesurfer.registerPlugin(RegionsPlugin.create());
-// wsRegions.addRegion({
-//   start: 19,
-//   content: "Marker",
-//   color: randomColor(),
-// });
-
 addEventListener("keydown", function (event) {
   if (event.key === " ") {
     wavesurfer.playPause();
+  } else if (event.key === "ArrowDown" || event.key === "Down") {
+    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    addMark(percentage);
+  } else if (event.key == "ArrowLeft") {
+    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    const closestPreviousMark = marks.reduce((prev, curr) => {
+      return curr < percentage ? curr : prev;
+    });
+    wavesurfer.seekTo(closestPreviousMark);
+  } else if (event.key == "ArrowRight") {
+    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    const nextMark = marks.find((mark) => mark > percentage);
+    if (nextMark) {
+      wavesurfer.seekTo(nextMark);
+    }
   }
 });
 
-addEventListener("keydown", function (event) {
-  if (event.key === "ArrowDown" || event.key === "Down") {
-    const time = wavesurfer.getCurrentTime();
-    console.log(time);
-    addMark(time);
+function addMark(percentage) {
+  if (!marks.includes(percentage)) {
+    const mark = document.createElement("div");
+    mark.classList.add("mark");
+    mark.style.left = `${percentage * 100}%`;
+    document.querySelector("#marks").appendChild(mark);
+    marks.push(percentage);
   }
-});
-
-function addMark(time) {
-  const mark = document.createElement("div");
-  mark.classList.add("mark");
-  mark.style.left = `${(time / wavesurfer.getDuration()) * 100}%`;
-  document.querySelector("#marks").appendChild(mark);
-  marks.push(time);
 }
