b9165a675fb417d72b33de54cf94e8800560da91
Owen Brooks <owen.h.brooks@gmail.com>
2024-02-11 17:27:50 +1100

    Add deletion of marks

--- wavesurfer.js
+++ wavesurfer.js
@@ -2,11 +2,11 @@ const wavesurfer = WaveSurfer.create({
   container: "#waveform",
   waveColor: "rgb(200, 0, 200)",
   progressColor: "rgb(100, 0, 100)",
-  cursorWidth: 3,
+  cursorWidth: 1,
   url: "sing.mp3",
 });
 
-const marks = [0.0];
+const marks = { 0: 0.0 };
 
 addEventListener("keydown", function (event) {
   if (event.key === " ") {
@@ -16,25 +16,46 @@ addEventListener("keydown", function (event) {
     addMark(percentage);
   } else if (event.key == "ArrowLeft") {
     const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
-    const closestPreviousMark = marks.reduce((prev, curr) => {
-      return curr < percentage ? curr : prev;
+    const prevMark = Object.entries(marks).reduce((prev, curr) => {
+      return curr[1] < percentage ? curr : prev;
     });
-    wavesurfer.seekTo(closestPreviousMark);
+    wavesurfer.seekTo(prevMark[1]);
   } else if (event.key == "ArrowRight") {
     const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
-    const nextMark = marks.find((mark) => mark > percentage);
-    if (nextMark) {
-      wavesurfer.seekTo(nextMark);
+    for (let mark in marks) {
+      if (marks[mark] > percentage) {
+        wavesurfer.seekTo(marks[mark]);
+        break;
+      }
     }
+  } else if (event.key == "ArrowUp" || event.key === "Up") {
+    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    console.log(marks);
+    const closestMark = Object.entries(marks).reduce((prev, curr) => {
+      return Math.abs(curr[1] - percentage) < Math.abs(prev[1] - percentage)
+        ? curr
+        : prev;
+    });
+
+    console.log(closestMark);
+    delete marks[closestMark[0]];
+    this.document.getElementById("marks").removeChild(
+      this.document.getElementById(`mark${closestMark[0]}`)
+    );
   }
 });
 
 function addMark(percentage) {
-  if (!marks.includes(percentage)) {
+  if (!Object.values(marks).includes(percentage)) {
+    let keys = Object.keys(marks);
+    let keyNums = keys.map((key) => Number(key));
+    let maxKey = Math.max(...keyNums);
+    const id = maxKey + 1;
     const mark = document.createElement("div");
     mark.classList.add("mark");
     mark.style.left = `${percentage * 100}%`;
+    mark.id = `mark${id}`;
     document.querySelector("#marks").appendChild(mark);
-    marks.push(percentage);
+    marks[id * 1] = percentage;
   }
 }
