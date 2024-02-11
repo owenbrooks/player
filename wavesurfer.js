e1996a40d3befcf84f739c96818dbb5b9fd23f9f
Owen Brooks <owen.h.brooks@gmail.com>
2024-02-11 20:37:03 +1100

    Factor out logic into functions

--- wavesurfer.js
+++ wavesurfer.js
@@ -8,90 +8,66 @@ const wavesurfer = WaveSurfer.create({
 
 const marks = [{ id: 0, time: 0.0 }];
 
-function findClosestIndex(arr, target) {
-  let closest = 0;
-  let closestDist = Math.abs(arr[0] - target);
-  for (let i = 1; i < arr.length; i++) {
-    const dist = Math.abs(arr[i] - target);
-    if (dist < closestDist) {
-      closest = i;
-      closestDist = dist;
-    }
-  }
-  return closest;
-}
-
 addEventListener("keydown", function (event) {
   if (event.key === " ") {
     wavesurfer.playPause();
   } else if (event.key === "ArrowDown" || event.key === "Down") {
-    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
-    addMark(percentage);
+    const playhead = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    addMark(playhead);
   } else if (event.key == "ArrowLeft") {
-    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
-    if (marks.length === 0) return;
-    if (marks.length === 1 && marks[0].time < percentage) {
-      wavesurfer.seekTo(marks[0].time);
-      return;
-    }
-    let closestMark = null;
-    for (let mark of marks) {
-      if (mark.time < percentage) {
-        closestMark = mark;
-      } else {
-        break;
-      }
-    }
-    if (closestMark !== null) {
-      wavesurfer.seekTo(closestMark.time);
-    }
+    const playhead = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    seekToPreviousMark(playhead);
   } else if (event.key == "ArrowRight") {
-    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
-    if (marks.length === 0) return;
-    if (marks.length === 1 && marks[0].time > percentage) {
-      wavesurfer.seekTo(marks[0].time);
-      return;
-    }
-    let closestMark = null;
-    for (let mark of [...marks].reverse()) {
-      if (mark.time > percentage) {
-        closestMark = mark;
-      } else {
-        break;
-      }
-    }
-    if (closestMark !== null) {
-      wavesurfer.seekTo(closestMark.time);
-    }
+    const playhead = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    seekToNextMark(playhead);
   } else if (event.key == "ArrowUp" || event.key === "Up") {
-    // TODO this is broken
     if (marks.length === 0) return;
-    const percentage = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
-    
-    // Find closest mark
-    const distances = marks.map((mark) => Math.abs(mark.time - percentage));
-    let closestIndex = 0;
-    let closestDist = distances[0];
-    for (let index = 0; index < distances.length; index++) {
-      if (distances[index] < closestDist) {
-        closestIndex = index;
-        closestDist = distances[index];
-      } 
-    }
-
-    // Remove from DOM
-    const closestMarkId = marks[closestIndex].id;
-    console.log(closestIndex, marks[closestIndex])
-    this.document
-      .getElementById("marks")
-      .removeChild(this.document.getElementById(`mark${closestMarkId}`));
-    // Remove from state
-    marks.splice(closestIndex, 1);
+    const playhead = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    removeClosestMark(playhead);
   }
 });
 
+function seekToPreviousMark(playhead) {
+  if (marks.length === 0) return;
+  if (marks.length === 1 && marks[0].time < playhead) {
+    wavesurfer.seekTo(marks[0].time);
+    return;
+  }
+  let closestMark = null;
+  for (let mark of marks) {
+    if (mark.time < playhead) {
+      closestMark = mark;
+    } else {
+      break;
+    }
+  }
+  if (closestMark !== null) {
+    wavesurfer.seekTo(closestMark.time);
+  }
+}
+
+function seekToNextMark(playhead) {
+  if (marks.length === 0) return;
+  if (marks.length === 1 && marks[0].time > playhead) {
+    wavesurfer.seekTo(marks[0].time);
+    return;
+  }
+  let closestMark = null;
+  for (let mark of [...marks].reverse()) {
+    if (mark.time > playhead) {
+      closestMark = mark;
+    } else {
+      break;
+    }
+  }
+  if (closestMark !== null) {
+    wavesurfer.seekTo(closestMark.time);
+  }
+}
+
 function addMark(percentage) {
   if (!marks.map((mark) => mark.time).includes(percentage)) {
+    // Compute new mark id
     let newId;
     if (marks.length === 0) {
       newId = 0;
@@ -111,6 +87,27 @@ function addMark(percentage) {
     mark.style.left = `${percentage * 100}%`;
     mark.id = `mark${newId}`;
     document.querySelector("#marks").appendChild(mark);
-
   }
+}
+
+function removeClosestMark(playhead) {
+    // Find closest mark to current position
+    const distances = marks.map((mark) => Math.abs(mark.time - playhead));
+    let closestIndex = 0;
+    let closestDist = distances[0];
+    for (let index = 0; index < distances.length; index++) {
+      if (distances[index] < closestDist) {
+        closestIndex = index;
+        closestDist = distances[index];
+      }
+    }
+
+    // Remove from DOM
+    const closestMarkId = marks[closestIndex].id;
+    console.log(closestIndex, marks[closestIndex]);
+    this.document
+      .getElementById("marks")
+      .removeChild(this.document.getElementById(`mark${closestMarkId}`));
+    // Remove from state
+    marks.splice(closestIndex, 1);
 }
